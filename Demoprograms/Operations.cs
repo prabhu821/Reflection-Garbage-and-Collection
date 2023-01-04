26b7b77926f44cb28392799faddbf0a0a0794ea9
rishiraj007 <rishiraj@mounttalent.com>
2023-01-04 23:24:28 +0530

    [Prabhu] Add: Lambda Expressions

---
+++ Demoprograms/Operations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demoprograms
+{
+    internal class Operations
+    {
+        public void RetreiveTopAge(List<Person> persons)
+        {
+            var data =persons.Where(x => x.Age > 60).ToList().Take(2);
+            foreach(var result in data)
+            {
+                Console.WriteLine("SSN: {0} Name: {1} Address: {2} Age: {3} ", result.SSN, result.Name, result.Address, result.Age);
+            }
+        }
+
+        public void RetreiveAllAge(List<Person> persons)
+        {
+            var data = persons.Where(x => x.Age > 13 && x.Age<18).ToList();
+            foreach (var result in data)
+            {
+                Console.WriteLine("SSN: {0} Name: {1} Address: {2} Age: {3} ",result.SSN,result.Name,result.Address,result.Age);
+            }
+        }
+
+        public void AverageAge(List<Person> persons)
+        {
+            var data = persons.Average(x => x.Age);
+            Console.WriteLine("\nAverage Age is: {0}",data);
+            
+        }
+    }
+}
--- Demoprograms/Program.cs
+++ Demoprograms/Program.cs
@@ -10,7 +10,31 @@
             //Collection.Dictinory();
             //Collection.Set();
 
-            ReflectionOnCustomer.ReflectionTest();
+            //ReflectionOnCustomer.ReflectionTest();
+
+            Operations operations = new Operations();
+            List<Person> persons = new List<Person>();
+            persons.Add(new Person() 
+            {  SSN =1, Name="Prabhu", Address="Moradabad",Age=24});
+            persons.Add(new Person()
+            { SSN = 2, Name = "Hemant", Address = "Delhi", Age = 16 });
+            persons.Add(new Person()
+            { SSN = 3, Name = "Vinesh", Address = "Ghaziabad", Age = 15 });
+            persons.Add(new Person()
+            { SSN = 4, Name = "Atul", Address = "Chandigarh", Age = 61 });
+            persons.Add(new Person()
+            { SSN = 5, Name = "Rita", Address = "Ajmer", Age = 63 });
+            persons.Add(new Person()
+            { SSN = 6, Name = "Akash", Address = "Deharadun", Age = 65 });
+            persons.Add(new Person()
+            { SSN = 7, Name = "Surbhi", Address = "Rajasthan", Age = 42 });
+            persons.Add(new Person()
+            { SSN = 8, Name = "Anita", Address = "Kanpur", Age = 17 });
+            Console.WriteLine("Person above age 60 are:");
+            operations.RetreiveTopAge(persons);
+            Console.WriteLine("\nPerson btw age 13 to 18 are:");
+            operations.RetreiveAllAge(persons);
+            operations.AverageAge(persons);
         }
     }
 }
